3389e2bad9686f6223684c19280fc83eedb028a6
E206554Q <elouan.toullec@univ-nantes.fr>
2022-11-30 11:32:09 +0100

    fix: import inutile

--- src/components/PaintingCard.js
+++ src/components/PaintingCard.js
@@ -1,7 +1,7 @@
 import { Card } from 'antd';
 import { useNavigate } from "react-router-dom";
 
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const PaintingCard = (props) => {
     const { Meta } = Card;
